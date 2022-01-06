a105a7c7945a788d95f8b288128c1e469d3ee43a
Parietic <70056597+Parietic@users.noreply.github.com>
2022-01-06 08:15:05 +0000

    Added arrays of strings for building fake facts

---
+++ MixedMessages.js
@@ -0,0 +1,24 @@
+// Store string arrays as global const's, keeps it neat, and it's no harm since this is just a small script
+// Message should be of the format: PREFIX FAKE_FACT SUFFIX
+const prefixes = [
+	// ['Did you know ', '?'], // Array prefix will use predefined suffix
+	// ['You should know ', '...'],
+	"It's common knowledge ",
+	"It's crazy to think ",
+	"The Government doesn't want you to know ",
+	"I can't believe you don't know ",
+];
+const suffixes = ['.', '!', '...'];
+
+const factBodies = [
+	'SUBJECT are just ADJECTIVE OBJECTS',
+	'SUBJECT were part of OBJECTS until DATE',
+	'SUBJECT have been used as a form of currency by OBJECTS',
+	'ADVERB VERB was made illegal in OBJECT after DATE',
+];
+
+// Following SVO, and SVIO word order
+const subjects = [];
+const indirectObjects = [];
+const verbs = [];
+const objects = [];
